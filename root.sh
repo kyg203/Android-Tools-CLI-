d61ad856f9cdd4cc95d433ef66294ffe6f9463eb
kyg203 <84691089+kyg203@users.noreply.github.com>
2021-06-03 20:33:16 -0400

    updated root.sh (beta)
    Updated script to latest version... Removes resource files (Magisk APK/ZIP and Lineage.img) and correctly installs Magisk without extra steps. Please comment any issues. OnePlus 8 only for now.

--- root.sh
+++ root.sh
@@ -1,15 +1,16 @@
-echo "You are about to root your phone using Magisk v23"
-cd ~/instantnoodle
-mkdir root
-cd root
+read -p "You are about to root your phone with Magisk (version 23). Press any button to continue."
+cd ~/instantnoodle-autotools-cli/root
+echo "Downloading Magisk and Lineage Recovery..."
 wget https://github.com/topjohnwu/Magisk/releases/download/v23.0/Magisk-v23.0.apk
+wget https://mirrorbits.lineageos.org/recovery/instantnoodle/20210529/lineage-18.1-20210529-recovery-instantnoodle.img
+mv lineage-18.1-20210529-recovery-instantnoodle.img recovery.img
+adb install Magisk-v23.0.apk
 mv Magisk-v23.0.apk magisk.zip
-wget https://mirrorbits.lineageos.org/recovery/instantnoodle/20210522/lineage-18.1-20210522-recovery-instantnoodle.img
-mv lineage-18.1-20210522-recovery-instantnoodle.img recovery.img
 adb reboot bootloader
 sudo fastboot flash recovery.img
 sudo fastboot reboot recovery
-echo "Go to Apply Update > Apply from ADB on your phone. When prompted, press Yes."
-read -p "Press any key to continue"
+read -p "Go to Apply Update > Apply from ADB on your phone. Press any key to continue."
 adb sideload magisk.zip
 read "you can now reboot your device. Press the back arrow button at the top and then press 'Reboot system now'"
+rm magisk.zip
+rm recovery.img

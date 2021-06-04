7e72f4e287fef0f6242604de2ab6fe67316caebe
kyg203 <84691089+kyg203@users.noreply.github.com>
2021-06-03 20:38:07 -0400

    Echo
    Removed a few "echo" commands to save space

--- setup.sh
+++ setup.sh
@@ -1,48 +1,15 @@
 #!/bin/bash
 
-echo "Setting up auto tools for the first time, please wait..."
-echo "Creating workspace for tools..."
-cd ~
-mkdir instantnoodle-autotools-cli
-cd instantnoodle-autotools-cli
-mkdir roms
-cd roms
-mkdir LineageOS
-mkdir FluidOS
-mkdir AICP
-mkdir crDroid
-mkdir EvolutionX
-mkdir HavocOS
-mkdir HyconOS
-mkdir MSM-Xtended
-mkdir OpenBeta
-mkdir ParanoidAndroid
-mkdir PixelExperience
-mkdir ShapeShift
-cd ../
-mkdir root
-mkdir apps
-mkdir apps/GCam
-
+echo "Setting up instantnoodle auto tools for the first time, please wait..."
 echo "installing tools"
-echo "setting up wget"
 sudo apt-get install wget
-echo "wget installed"
-echo "installing the latest version of python"
 sudo apt-get install python3
 sudo apt-get install python3.8
-echo "Python installed."
-echo "installing pip"
 sudo apt-get install python3-pip
-echo "Pip installed."
-echo "installing pip resources..."
 pip3 install protobuf
 pip3 install bsdiff4
-echo "Downloading payload_dumper"
 wget https://github.com/vm03/payload_dumper/archive/refs/heads/master.zip
-echo "Downloaded. Decompressing"
 unzip master.zip
-echo "Downloading ADB and fastboot tools"
 wget https://dl.google.com/android/repository/platform-tools-latest-linux.zip
 unzip platform-tools-latest-linux.zip
 echo "Download finished. Removing temporary files..."
@@ -55,5 +22,5 @@ sudo cp fastboot /usr/bin
 echo "ADB and Fastboot setup."
 sudo chmod +x autotools.sh
 echo "To begin using autotools, type ./autotools.sh in the instantnoodle-autotools-cli folder"
-cd instantnoodle-autotools-cli
+cd ~/instantnoodle-autotools-cli
 done

3cd3177ceff098da24eb0fc66509457d6b9c61e5
kyg203 <84691089+kyg203@users.noreply.github.com>
2021-06-03 22:18:22 -0400

    Create pixelize-me.sh

---
+++ rom/pixelexperience/pixelize-me.sh
@@ -0,0 +1,19 @@
+wget -O pe.zip https://link.us1.storjshare.io/s/jxhmwyjvyhoi4aw6qyr7xc6efgca/pixelexperience8a55844831018ff94dc3320e472ff3cb/instantnoodle/PixelExperience_Plus_instantnoodle-11.0-20210518-1444-OFFICIAL.zip?download
+unzip pe.zip
+mv payload.bin ../../payload_dumper-master
+rm care_map.pb
+rm -r META-INF
+rm payload_properties.txt
+rm pe.zip
+cd ../../payload_dumper-master
+python3 payload_dumper.py payload.bin
+cd output
+read -p "Before Pixelization, please backup your phone!"
+adb reboot fastboot
+sudo bash fastboot-img-flash.sh
+sudo fastboot reboot
+cd ../
+rm payload.bin
+cd output
+rm *.img
+done

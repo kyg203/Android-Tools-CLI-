1f53d40f62319810cfc8debd0660a234ffde6896
kyg203 <84691089+kyg203@users.noreply.github.com>
2021-06-15 16:17:04 -0400

    Update discover-your-lineage.sh

--- rom/lineageos/discover-your-lineage.sh
+++ rom/lineageos/discover-your-lineage.sh
@@ -1,6 +1,6 @@
 echo "Downloading Lineage OS v18.1 (Android 11)"
-wget https://mirrorbits.lineageos.org/full/instantnoodle/20210522/lineage-18.1-20210529-nightly-instantnoodle-signed.zip
-mv lineage-18.1-20210529-nightly-instantnoodle-signed.zip lineage.zip
+wget https://mirrorbits.lineageos.org/full/instantnoodle/20210612/lineage-18.1-20210612-nightly-instantnoodle-signed.zip
+mv lineage-18.1-20210612-nightly-instantnoodle-signed.zip lineage.zip
 unzip lineage.zip
 mv payload.bin ../../payload_dumper-master/
 rm care_map.pb
@@ -10,14 +10,13 @@ rm payload_properties.txt
 cd ../../payload_dumper-master
 python3 payload_dumper.py payload.bin
 read -p "Extracting images... Please take a minute to backup your phone, don't forget WhatsApp! Press any button to continue."
+rm payload.bin
 cd output
 echo 'Rebooting to fastbootd"
 adb reboot fastboot
-sudo bash ~/instantnoodle-autotools-cli/rom/fastboot-img-flash.sh
+sudo bash ../fastboot-flash.sh
 sudo bash gapps.sh
 sudo fastboot reboot
 echo "Rebooting and cleaning up disk space..."
 rm *.img
-cd ../
-rm payload.bin
 echo "All done!"

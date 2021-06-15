043c518e178daa5a26cc3e8841eb22d3bbe3ec03
kyg203 <84691089+kyg203@users.noreply.github.com>
2021-06-15 17:02:40 -0400

    Update shapeshift-my-device.sh

--- rom/shapeshift/shapeshift-my-device.sh
+++ rom/shapeshift/shapeshift-my-device.sh
@@ -1,6 +1,7 @@
+cd rom/shapeshift
 echo "Downloading ShapeShift OS v2.6 Sceptile for OnePlus 8 (instantnoodle)"
 wget -O ss.zip https://downloads.sourceforge.net/project/shapeshiftos/instantnoodle/ShapeShiftOS-2.6-Sceptile-OFFICIAL-instantnoodle-20210517-1120.zip?ts=gAAAAABguXso1WjsJlJGJFR2YUJrNdaVfnzx4G36T7djQJPDwzbKwWpWeC7eS9Fdph4e-zwY0zIKuFCI1BIUA0b2g6NAxDbwug%3D%3D&r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fshapeshiftos%2Ffiles%2Finstantnoodle%2FShapeShiftOS-2.6-Sceptile-OFFICIAL-instantnoodle-20210517-1120.zip%2Fdownload
-read -p "Press any key to continue after the ROM has downloaded"
+read -p "press any key to continue after ROM has finished downloading"
 echo "Extracting payload.bin file from .zip"
 unzip ss.zip
 rm -r META-INF
@@ -11,12 +12,15 @@ mv payload.bin ../../payload_dumper-master
 cd ../../payload_dumper-master
 python3 payload_dumper.py payload.bin
 cd output
-echo "Rebooting to fastbootd and beginning flashing process."
+echo "Rebooting to fastboot and beginning flashing process."
 adb reboot fastboot
-sudo bash ../fastboot-flash.sh
-echo "Done flashing. Cleaning up desktop"
+sudo fastboot erase system
+sudo fastboot erase userdata
+sudo bash ../../rom/fastboot-flash.sh
+echo "ShapeShift OS successfully installed!"
 rm *.img
 cd ../
 rm payload.bin
 cd ../
-echo "done"
+sudo fastboot reboot recovery
+echo "factory reset your device and then reboot"

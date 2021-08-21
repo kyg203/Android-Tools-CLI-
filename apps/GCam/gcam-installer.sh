0b21bcfb3cb80edd74662df8488815aec7a9eefe
kyg203 <84691089+kyg203@users.noreply.github.com>
2021-08-21 16:08:57 -0400

    Update gcam-installer.sh

--- apps/GCam/gcam-installer.sh
+++ apps/GCam/gcam-installer.sh
@@ -8,7 +8,7 @@ echo "$title"
 PS3="$prompt"
 select opt in "${options[@]}" "Quit"; do 
 case "$REPLY" in
-    1) echo "Preparing to instal GCam v7.3 on your device"
+    1) echo "Preparing to install GCam v7.3 on to your device..."
        echo "Downloading GCam v7.3 APK"
        wget https://www.theproplayer.com/KyG/7.3.apk
        wget https://www.theproplayer.com/KyG/nrG.xml
@@ -16,26 +16,36 @@ case "$REPLY" in
        echo "GCam v7.3 downloaded. Installing..."
        adb install 7.3.apk
        adb push nrG.xml /storage/emulated/0/GCam/Configs7/nrg.xml
-       echo "GCam v7.3 installed! Press 3 to exit."
+       echo "GCam v7.3 installed!"
+       rm *.apk
+       rm *.xml
        break;;
        ;;
-    2) echo "Installing GCam v8.1-1.2"
+    2) echo "Preparing to install PX Camera to your device..."
        echo "Downloading PX Camera APK"
        wget https://www.theproplayer.com/KyG/PX.apk
        read -p "Press anywhere to continue once PX Camera has downloaded"
        echo "PX Camera downloaded. Installing..."
        adb install PX.apk
-       echo "PX Camera installed! Press 3 to exit."
+       echo "PX Camera installed!"
+       rm *.apk
        break;;
        ;;
-    3) echo "Installing both GCam v7.3 and PX Camera"
+    3) echo "Preparing to install PX Camera and GCam v7.3 on to your device..."
+       echo "Downloading PX Camera..."
        wget https://www.theproplayer.com/KyG/PX.apk
+       echo "Downloading GCam v7.3..."
        wget https://www.theproplayer.com/KyG/7.3.apk
+       echo "Downloading config for GCam v7.3..."
+       wget https://www.theproplayer.com/KyG/nrG.xml
+       echo "Pushing config to device."
+       adb push nrG.xml /storage/emulated/0/GCam/Configs7/nrg.xml
+       echo "Installing apps..."
        adb install PX.apk
        adb install 7.3.apk
-       wget https://www.theproplayer.com/KyG/nrG.xml
-       adb push nrG.xml /storage/emulated/0/GCam/Configs7/nrg.xml
        echo "Both apps installed."
+       rm *.xml
+       rm *.apk
        break;;
        ;;
     $((${#options[@]}+1))) echo "Exiting..."; 
